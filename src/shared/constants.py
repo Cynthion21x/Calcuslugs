4c741e6214ae3297d14c1f0a52b7e93deb02382d
Cynthion21x <ice100k@outlook.com>
2023-08-17 13:03:07 +0100

    initial commit

---
+++ src/shared/constants.py
@@ -0,0 +1,22 @@
+import pygame
+
+# Colours
+
+class Colours:
+
+    CERISE = pygame.Color(222, 49, 99)
+    BLACK = pygame.Color(0, 0, 0)
+    WHITE = pygame.Color(255, 255, 255)
+
+# Display
+
+SCREEN_NAME = "game"
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+FRAME_RATE = 60
+
+# Network options
+
+LOCALHOST = "127.0.0.1"
+DEFAULT_PORT = "1700"
