29233e0b845646b6e75d2cfb9671c103c22d17da
cynthion21x <cynthion21x@gmail.com>
2024-06-28 11:04:28 +0100

    fresh new generator function

--- src/core/entities/tilemap.py
+++ src/core/entities/tilemap.py
@@ -1,5 +1,6 @@
 import src.core.entities.obstacle as obstacle
 import src.math.vectors as v
+import src.shared.constants as c
 import math
 
 class Tilemap:
@@ -16,7 +17,9 @@ class Tilemap:
 
             for y in range(height):
 
-                index = 15 - math.sin((x+params[3]) / params[0]) + math.sin((x+params[4]) / params[1]) + math.sin((x+params[5]) / params[2])
+                xS = x + c.GAME_WIDTH / 2
+
+                index = (c.GAME_HEIGHT * 0.75) - (params[6] *math.sin((xS+params[3]) / params[0]) + params[7] * math.sin((xS+params[4]) / params[1]) + params[8] * math.sin((xS+params[5]) / params[2]))
 
                 if (y > index):
 
